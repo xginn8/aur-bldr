736740d6044a8bd89af95000d785af97ed64ade0
Matthew McGinn <mamcgi@gmail.com>
2019-12-31 10:55:55 -0500

    update build.sh to accommodate package cleanups
    Signed-off-by: Matthew McGinn <mamcgi@gmail.com>

--- README.md
+++ README.md
@@ -11,16 +11,17 @@ mkosi/Docker config for building clean Arch Linux containers for packaging
 
 1. `docker build .`
 1. `docker run -e AUR_PACKAGE=name -it {{hash of build container}}`
-
 # To rebuild:
 `# mkosi --force`
 
 # To regenerate travis:
-`$ cat travis.base.yml <(AUR_MAINTAINER=gin078 python aur-search.py) > .travis.yml`
+`$ cat travis.base.yml travis.comaintainers.yml <(AUR_MAINTAINER=gin078 python aur-search.py) > .travis.yml`
 
 ## TODO:
 
 1. This does not work with packages that the `AUR_MAINTAINER` is a comaintainer.
-+ Workaround: use the travis.comaintainers.yml stub
-1. Create build container to use as a base (weekly cron job via Travis?)
++ Workaround: use the [travis.comaintainers.yml](./travis.comaintainers.yml) stub
+1. Create build container to use as a base (monthly cron job via Travis?)
 1. Some packages (`xv`, `bitbake`) use FTP to get source code, which isn't well-supported by Travis.
+1. Set up repos with default `.gitignore`, hooks for `mksrcinfo`, etc
+1. `bitbake` also needs git setup (releases aren't ordered properly)
--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ updpkg () {
     echo "getting latest version from github"
     local URL
     local VERSION
-    URL=$(awk -F= '/url=/ {print $2}' PKGBUILD | sort | tail -n1 | sed -e 's@https://github.com@https://api.github.com/repos@' -e "s/[\'\"]//g" -e 's@$@/releases/latest@')
+    URL=$(awk -F= '/url=.*github.com.*/{print $2}' PKGBUILD | sed -e 's@https://github.com@https://api.github.com/repos@' -e "s/[\'\"]//g" -e 's@$@/releases/latest@')
+    echo "URL: ${URL}"
     VERSION=$(curl -XGET -H "Accept: application/vnd.github.v3+json" "${URL}" | jq -r '.tag_name | sub("^v";"")')
     if [ $? -ne 0 ]; then
-        URL=$(awk -F= '/url=/ {print $2}' PKGBUILD | sort | tail -n1 | sed -e 's@https://github.com@https://api.github.com/repos@' -e "s/[\'\"]//g" -e 's@$@/tags@')
+        URL=$(awk -F= '/url=.*github.com.*/{print $2}' PKGBUILD | sed -e 's@https://github.com@https://api.github.com/repos@' -e "s/[\'\"]//g" -e 's@$@/tags@')
         VERSION=$(curl -XGET -H "Accept: application/vnd.github.v3+json" "${URL}" | jq -r '.[0].name | sub("^v";"")')
 
     fi
@@ -28,14 +29,12 @@ updpkg () {
     then
             sed -i "s/^pkgrel=[0-9\.]*$/pkgrel=1/g" PKGBUILD
             sed -i "s/^pkgver=[0-9\.]*$/pkgver=${VERSION}/g" PKGBUILD
-            git config user.email "${AUR_MAINTAINER_EMAIL}"
-            git config user.name "${AUR_MAINTAINER_NAME}"
-            git config user.username "${AUR_MAINTAINER_USERNAME}"
+            exit
             updpkgsums && mksrcinfo && makepkg -sriC --noconfirm --noprogressbar && \
                 git add . && git commit -sm "update version to v${VERSION}"
             echo "New version available (${VERSION})!" && exit 1
     fi
-    makepkg -sri --noconfirm --noprogressbar && namcap ./*tar.xz && namcap PKGBUILD
+    (makepkg -sri --noconfirm --noprogressbar && namcap ./*tar.xz && namcap PKGBUILD) || yay --noconfirm --noprogressbar "${AUR_PACKAGE}" && cd "~/.cache/yay/${AUR_PACKAGE}" && namcap ./*tar.xz && namcap PKGBUILD
 }
 
 sudo su -c 'reflector -n10 > /etc/pacman.d/mirrorlist'
@@ -55,4 +54,7 @@ else
     git clone "https://aur.archlinux.org/${AUR_PACKAGE}.git" "${BUILD_DIR}/${AUR_PACKAGE}"
 fi
 cd "${AUR_PACKAGE}" || exit 1
+git config user.email "${AUR_MAINTAINER_EMAIL}"
+git config user.name "${AUR_MAINTAINER_NAME}"
+git config user.username "${AUR_MAINTAINER_USERNAME}"
 updpkg
