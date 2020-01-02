e0f24c1891e204e79680b71fbf95e12b67100801
Matthew McGinn <mamcgi@gmail.com>
2020-01-02 00:09:07 -0500

    update build script
    Signed-off-by: Matthew McGinn <mamcgi@gmail.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ updpkg () {
     VERSION=$(curl -XGET -H "Accept: application/vnd.github.v3+json" "${URL}" | jq -r '.tag_name | sub("^v";"")')
     if [ $? -ne 0 ]; then
         URL=$(awk -F= '/url=.*github.com.*/{print $2}' PKGBUILD | sed -e 's@https://github.com@https://api.github.com/repos@' -e "s/[\'\"]//g" -e 's@$@/tags@' | head -n1)
-        VERSION=$(curl -XGET -H "Accept: application/vnd.github.v3+json" "${URL}" | jq -r '.[0].name | sub("^v";"")')
+        VERSION=$(curl -XGET -H "Accept: application/vnd.github.v3+json" "${URL}" | jq -Sr '.[].name | sub("^v";"") | match("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"; "g") | .string' | head -n1)
 
     fi
     local OLD_VERSION
@@ -37,16 +37,13 @@ updpkg () {
     namcap ./*tar.xz && namcap ./PKGBUILD
 }
 
-sudo su -c 'reflector -n10 > /etc/pacman.d/mirrorlist'
 if [ -z "${AUR_PACKAGE}" ]; then
     echo "need to define \$AUR_PACKAGE, exiting"
     exit 1
 fi
 
+sudo su -c 'reflector -n10 > /etc/pacman.d/mirrorlist'
 cd /home/bldr/
 yay -G "${AUR_PACKAGE}"
 cd ${AUR_PACKAGE}
-git config user.email "${AUR_MAINTAINER_EMAIL}"
-git config user.name "${AUR_MAINTAINER_NAME}"
-git config user.username "${AUR_MAINTAINER_USERNAME}"
 updpkg
