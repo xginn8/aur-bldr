010b2df52b2faa912aceb4a7cd77c0bae3d12e09
Matthew McGinn <mamcgi@gmail.com>
2020-01-09 16:22:53 -0500

    update travis setup
    Signed-off-by: Matthew McGinn <mamcgi@gmail.com>

--- aur-search.py
+++ aur-search.py
@@ -5,4 +5,4 @@ import aur
 
 for i in aur.msearch(os.getenv('AUR_MAINTAINER')):
     if (i.name == i.package_base):
-        print("- AUR_PACKAGE={}".format(i.name))
+        print("        - AUR_PACKAGE={}".format(i.name))
